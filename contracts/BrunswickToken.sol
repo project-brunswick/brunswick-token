a2948ebe33d11217e5f30db8f3a1e6821ed513b9
Chris Zheng <chris@statstrade.io>
2024-03-10 07:16:52 +1100

    adding token

--- contracts/BrunswickToken.sol
+++ contracts/BrunswickToken.sol
@@ -3,16 +3,21 @@ pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC20Base.sol";
 
-contract StatstradeClassA is ERC20Base {
+contract BrunswickToken is ERC20Base {
       constructor(
-        address _defaultAdmin,
         string memory _name,
         string memory _symbol
     )
         ERC20Base(
-            _defaultAdmin,
+            msg.sender,
             _name,
             _symbol
         )
-    {}
+    {
+        _mint(payable(msg.sender), 1000000000);
+    }
+    
+    function decimals() public view override returns (uint8) {
+        return 0;
+    }
 }
